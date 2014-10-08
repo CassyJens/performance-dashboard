667f9d83a65c96f13c5cf66504fdd490a1ea2ce2
Cassy Jens <jens.cass@gmail.com>
2014-10-07 21:29:11 -0700

    graph script

---
+++ scripts/graphs.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    var data = [1, 2, 3, 10, 11, 12, 20, 21, 22, 30, 40];
+
+    new Contour({
+        el: '.graph',
+    })
+    .cartesian()
+    .line(data)
+    .render();
+});
