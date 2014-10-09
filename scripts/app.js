734ba0dbb2a61737141c63937da351baf73b4148
Lotus08 <chinmay.adhvaryu@gmail.com>
2014-10-08 22:13:14 -0700

    Added new odometers

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   var energySavingsOd = createOdometer({
       el: '#energy-savings-od',
-      startingVal: 485686484,
+      startingVal: 485686544,
       increment: 15.2
   });
 
@@ -9,6 +9,18 @@ $(document).ready(function () {
       el: '#c02-savings-od',
       startingVal: 295827,
       increment: 4.7
+  });
+  
+   var caryears = createOdometer({
+      el: '#caryears-savings-od',
+      startingVal: 56890,
+      increment: 0.09
+  });
+  
+	var sites = createOdometer({
+      el: '#sites-od',
+      startingVal: 212,
+      increment: 0
   });
 
 });
