3dfa1dfa25f272316c8d1b7fbdbea36a119321ce
Cassy Jens <jens.cass@gmail.com>
2014-10-08 21:08:18 -0700

    load visualizations from JSON

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,9 @@
 $(document).ready(function () {
-  var energySavingsOd = createOdometer({
-      el: '#energy-savings-od',
-      startingVal: 485686484,
-      increment: 15.2
+  $.getJSON('config/visualizations.json', function (data) {
+    $.each(data, function() {
+      $.each(this, function(k, v) {
+        console.log(k, v);
+      });
+    });
   });
-
-  var CO2SavingsOd = createOdometer({
-      el: '#c02-savings-od',
-      startingVal: 295827,
-      increment: 4.7
-  });
-
 });
