b4398bcf3f186dc819920a31fc3ae3fd726e7478
Lotus08 <chinmay.adhvaryu@gmail.com>
2014-10-09 13:24:11 -0700

    changes to text style, vo.1 complete

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,18 @@ $(document).ready(function () {
       startingVal: 486255844,
       increment: 15.2
   });
+  
+  var bladeserver = createOdometer({
+      el: '#bladeserver-od',
+      startingVal: 2265,
+      increment: 0.000007
+  });
+
 
   var CO2SavingsOd = createOdometer({
       el: '#c02-savings-od',
-      startingVal: 295827,
-      increment: 4.7
+      startingVal: 268609610,
+      increment: 0.84
   });
   
    var caryears = createOdometer({
@@ -20,6 +27,24 @@ $(document).ready(function () {
 	var sites = createOdometer({
       el: '#sites-od',
       startingVal: 212,
+      increment: 0
+  });
+  
+  var sqft = createOdometer({
+      el: '#sqft-od',
+      startingVal: 11811640,
+      increment: 0
+  });
+  
+  var acunit = createOdometer({
+      el: '#acunit-od',
+      startingVal: 4298,
+      increment: 0
+  });
+  
+  var data = createOdometer({
+      el: '#data-od',
+      startingVal: 67,
       increment: 0
   });
 
